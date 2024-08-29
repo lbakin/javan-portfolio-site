db2a9ac20f685a25c79bcb38d61ef29dd91100bf
Lev Bakin <lev@onapproval.com>
2024-08-28 17:23:57 -0700

    feat: about me section changes

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -3,10 +3,10 @@ export default function About() {
     <section className="p-8 bg-blue-900 text-white flex flex-row justify-between">
       <div className="">
         <h2 className="text-2xl font-bold mb-4">About</h2>
-        <p>Javan Ivey likes long walks on the beach. Why? Who`&apos;`s asking?</p>
+        <p>Javan Ivey likes long walks on the beach. Why? Who&apos;s asking?</p>
       </div>
       <div>
-        <img src="images/StopMotion8.png" alt="portrait of javan" className="w-36" />
+        <img src="images/AboutPic.png" alt="portrait of javan" className="w-36" />
       </div>
     </section>
   );
