dd524e78c7d9928d002ca5bb3f1fff73c77c66f8
Lev Bakin <lev@onapproval.com>
2024-08-29 15:40:49 -0700

    styling and spacing changes in portfolio

--- src/app/components/PortfolioSection.js
+++ src/app/components/PortfolioSection.js
@@ -9,18 +9,18 @@ export default function PortfolioSection({ title, reelUrls, images, id, sectionS
   
   const videoRefs = useRef(reelUrls.map(() => null));
 
-  const handleFullScreen = (index) => {
-    const videoRef = videoRefs.current[index];
-    if (videoRef) {
-      if (videoRef.requestFullscreen) {
-        videoRef.requestFullscreen();
-      } else if (videoRef.webkitRequestFullscreen) {
-        videoRef.webkitRequestFullscreen(); // For Safari
-      } else if (videoRef.msRequestFullscreen) {
-        videoRef.msRequestFullscreen(); // For IE/Edge
-      }
-    }
-  };
+  // const handleFullScreen = (index) => {
+  //   const videoRef = videoRefs.current[index];
+  //   if (videoRef) {
+  //     if (videoRef.requestFullscreen) {
+  //       videoRef.requestFullscreen();
+  //     } else if (videoRef.webkitRequestFullscreen) {
+  //       videoRef.webkitRequestFullscreen(); // For Safari
+  //     } else if (videoRef.msRequestFullscreen) {
+  //       videoRef.msRequestFullscreen(); // For IE/Edge
+  //     }
+  //   }
+  // };
 
   const openLightbox = (image) => {
     setCurrentImage(image);
@@ -42,34 +42,40 @@ export default function PortfolioSection({ title, reelUrls, images, id, sectionS
     <section id={id} className={`py-8 px-12 ${sectionStyle}`}>
       <h2 className={`text-2xl text-center font-bold mb-8 sticky top-0 ${sectionStyle} text-black py-4 z-10`}>{title}</h2>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mb-12">
         {reelUrls.map((reelUrl, index) => (
           <div key={index} className="relative w-full pb-56.25% h-0">
             <video ref={videoRefs.current[index]} controls className="absolute top-0 left-0 w-full h-full object-cover">
               <source src={reelUrl} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
-            <button
+            {/* <button
               onClick={() => handleFullScreen(index)}
               className="absolute top-2 right-2 p-2 bg-gray-700 bg-opacity-75 text-white rounded hover:bg-gray-600"
             >
               {'[ ]'}
-            </button>
+            </button> */}
           </div>
         ))}
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
         {images.map((image, index) => (
-          <div
-            key={index}
-            className="relative w-full h-64 lg:h-96 bg-indigo-300 overflow-hidden group cursor-pointer"
-            onClick={() => openLightbox(image)}
-          >
-            <img src={image.src} alt={image.alt} className="w-full h-full object-cover md:group-hover:opacity-75 transition-opacity duration-300" />
-            <div className="absolute inset-x-0 bottom-0 h-1/3 bg-black bg-opacity-50 flex items-center justify-center text-white text-lg px-4 md:opacity-0 md:group-hover:opacity-100 opacity-1000 group-hover:opacity-100 transition-opacity duration-300">
-              {image.text}
+          <div key={index} className="relative group cursor-pointer">
+            <div className="relative w-full h-64 lg:h-96 bg-indigo-300 overflow-hidden ">
+              <img
+                src={image.src}
+                alt={image.alt}
+                className="w-full h-full object-cover md:group-hover:opacity-75 transition-opacity duration-300 shadow-xl"
+              />
+              <div className="absolute inset-x-0 bottom-0 h-1/3 bg-black bg-opacity-50 flex items-center justify-center text-white text-lg px-4 md:opacity-0 md:group-hover:opacity-100 opacity-1000 group-hover:opacity-100 transition-opacity duration-300">
+                {image.text}
+              </div>
             </div>
+            {/* Shadow below the image */}
+            {/* <div className="absolute inset-x-0 h-12 bg-cover bg-no-repeat pointer-events-none"
+                 style={{ backgroundImage: `url('images/below-shadow.png')`, height: '20%', backgroundSize: 'cover', backgroundPosition: 'bottom center' }}>
+            </div> */}
           </div>
         ))}
       </div>
